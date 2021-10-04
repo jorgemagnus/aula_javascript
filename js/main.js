ebf3f1503cab37f3f23c67b6bed423df03cb508e
jorgemagnus <jorgemagnus_rn@hotmail.com>
2021-10-03 22:56:13 -0300

    Primeiro commit aula_javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var nome = "Jorge Magnus"
+var idade = 46;
+var idade2 = 10;
+var idade3 = idade + idade2;
+var frase ="Japão é o melhor time do mundo.";
+//para comentar uma linha
+/*
+  usa para comentar mais de uma linha.
+*/
+
+//somei o idade + idade2
+//alert("Sr(a) "+ nome+" tem "+idade3+" anos");
+
+//para ver as saídas do comando console no navegador, F12 , console.
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(idade * idade2);
+
